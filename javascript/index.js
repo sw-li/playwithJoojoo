113de4628de900dd91e8489be2f996c824ef0a3c
sw-li <lishengwei90@gmail.com>
2022-11-23 23:19:01 +0100

    basic DOM operations

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
-document.onclick = (e) => console.log(e.target + " is clicked")
+const gameDivs = [...document.getElementsByClassName("gameDiv")]
+
+window.onload = (e) => {
+    gameDivs.map(game => game.style.display = "none")
+}
+
+
+document.onclick = (e) => {
+    if(e.target.classList.contains("toAli")) {
+            document.getElementById("entries").style.display = "none"
+            document.getElementById("gameAli").style.display = "flex"
+        }
+    if(e.target.classList.contains("toShengwei")) {
+            document.getElementById("entries").style.display = "none"
+            document.getElementById("gameShengwei").style.display = "flex"
+        }
+    if(e.target.classList.contains("toJoojoo")) {
+            document.getElementById("entries").style.display = "none"
+            document.getElementById("gameJoojoo").style.display = "flex"
+        }
+    if(e.target.id == "backBtn"){
+        document.getElementById("entries").style.display= "flex"
+        gameDivs.map(game => game.style.display = "none")
+    }
+}
+
---
+++ javascript/playerAndfoods.js
@@ -0,0 +1,7 @@
+class Composant{
+    constructor(x,y,nature = "meat"){
+        this.x = x
+        this.y = y
+        this.nature = nature
+    }
+}
