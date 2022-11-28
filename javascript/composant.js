15131527672d103e3104ea7e67cc1d14c04aecb6
Shengwei LI <antoniovvtonyvv@gmail.com>
2022-11-28 16:51:58 +0100

    -add new avatar images

---
+++ javascript/composant.js
@@ -0,0 +1,11 @@
+
+ class Composant {
+    constructor(img,x,y,point,speed){
+        this.img = img
+        this.x = x
+        this.y = y
+        this.poit = point
+        this.speed = speed
+    }
+
+  }
