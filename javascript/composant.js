90e1b1679838aad9b1c8180926b40b7cf4f7a67e
Shengwei LI <antoniovvtonyvv@gmail.com>
2022-12-02 16:28:54 +0100

    image class created

--- javascript/composant.js
+++ javascript/composant.js
@@ -39,9 +39,102 @@ class Composant{
   }
 }
 
+class imgComposant extends Composant{
+    constructor(x,y,color,speedFactor=1){
+        super(x,y,color,speedFactor)
+        this.w = 20
+        this.h = 20
+        this.bufferSize = 2
+        this.speedX = 2* positifOrNegatif()
+        this.speedY = 2
+        this.speedFactor = speedFactor
+    }
+    update(){
+        let ctx = document.querySelector("canvas").getContext("2d")
+        this.newPos()
+        ctx.drawImage(randomImg(this.color), this.x,this.y,this.w,this.h)
+    }
+}
+
+
+
 function positifOrNegatif(){
   let index = Math.random()
   if(index>0.5) return 1
   return -1
 }
 
+
+const veggieAndFruit = [
+    { name: "Apple", img: "Apple.png", color: "red"},
+    { name: "Banana", img: "Banana.png", color: "red"},
+    { name: "Beet", img: "Beet.png", color: "red"},
+    { name: "Berries", img: "Berries.png", color: "red"},
+    { name: "Carrote", img: "Carrote.png", color: "red"},
+    { name: "Cherry", img: "Cherry.png", color: "red"},
+    { name: "Grapes", img: "Grapes.png", color: "red"},
+    { name: "Lemon", img: "Lemon.png", color: "red"},
+    { name: "Mushrooms1", img: "Mushrooms1.png", color: "red"},
+    { name: "Mushrooms2", img: "Mushrooms2.png", color: "red"},
+    { name: "Mushrooms3", img: "Mushrooms3.png", color: "red"},
+    { name: "Orange", img: "Orange.png", color: "red"},
+    { name: "PinePotatos", img: "PinePotatos.png", color: "red"},
+    { name: "Potatos", img: "Potatos.png", color: "red"},
+    { name: "Radish", img: "Radish.png", color: "red"},
+    { name: "Strawberry", img: "Strawberry.png", color: "red"},
+    { name: "Watermelon", img: "Watermelon.png", color: "red"},
+]
+
+const meatAndFish =[
+    { name: "Bacon", img: "Bacon.png", color: "green" },
+    { name: "Bacon_dish", img: "Bacon_dish.png", color: "green" },
+    { name: "Burger", img: "Burger.png", color: "green" },
+    { name: "Burger_1", img: "Burger_1.png", color: "green" },
+    { name: "Drumstick", img: "Drumstick.png", color: "green" },
+    { name: "Cheese", img: "Cheese.png", color: "green" },
+    { name: "Dumplings", img: "Dumplings.png", color: "green" },
+    { name: "Egg", img: "Egg.png", color: "green" },
+    { name: "Egg2", img: "Egg2.png", color: "green" },
+    { name: "Hotdog", img: "Hotdog.png", color: "green" },
+    { name: "Meatball", img: "Meatball.png", color: "green" },
+    { name: "Pizza", img: "Pizza.png", color: "green" },
+    { name: "Pizza 2", img: "Pizza 2.png", color: "green" },
+    { name: "Roti1", img: "Roti1.png", color: "green" },
+    { name: "Roti2", img: "Roti2.png", color: "green" },
+    { name: "Sausage", img: "Sausage.png", color: "green" },
+    { name: "Steak", img: "Steak.png", color: "green" },
+    { name: "Steak1", img: "Steak1.png", color: "green" },
+    { name: "Steak_dish", img: "Steak_dish.png", color: "green" },
+    { name: "Shrimp1", img: "Shirmp1.png", color: "green" },
+    { name: "Shrimp2", img: "Shirmp2.png", color: "green" },
+    { name: "Shrimp3", img: "Shirmp3.png", color: "green" },
+    { name: "Sushi1", img: "Sushi1.png", color: "green" },
+    { name: "Sushi2", img: "Sushi2.png", color: "green" },
+    { name: "Sushi3", img: "Sushi3.png", color: "green" },
+    { name: "Sushi4", img: "Sushi4.png", color: "green" },
+    { name: "Turkey", img: "Turkey.png", color: "green" },
+    { name: "Cheese", img: "Cheese.png", color: "green"},
+    { name: "Taco", img: "Taco.png", color: "green"},
+]
+
+function randomImg(color){
+    const img = new Image()
+    let imgName = ""
+    switch(color){
+        case "green":
+        imgName = randomMemberOfArray(meatAndFish).name
+        case "red":
+        imgName = randomMemberOfArray(veggieAndFruit).name
+    }
+    img.src = "./images/foods/"+imgName +".png"
+    return img
+}
+
+function randomMemberOfArray(arr){
+    if(arr == undefined) return undefined
+    if(arr.length>1){
+        return arr[Math.floor(Math.random()*arr.length)]
+    }else{
+        return arr[0]
+    }
+}
